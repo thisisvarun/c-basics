bc27909b8fae6712e450b435d56233b61f3c619e
thisisvarun <96563987+thisisvarun@users.noreply.github.com>
2021-12-23 11:56:51 +0530

    Add files via upload

---
+++ area of square.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+int a,ar,pr;
+printf("side = ");
+scanf("%d",&a);
+	
+ar=a*a;
+pr=4*a;
+	
+printf("Area is : %d \n",ar);
+printf("Perimeter is : %d",pr);
+return 0;
+}
